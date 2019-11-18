e435cd835ef4de771a3070af8d3aaafb31bc6292
Shuhua <nus.gao.shuhua@gmail.com>
2019-11-18 22:05:10 +0800

    correct the comments of read_network

--- src/algorithm/utils.py
+++ src/algorithm/utils.py
@@ -6,20 +6,18 @@ Date: 2019/10/25
 """
 from typing import Tuple, List
 
-
 def read_network(file: str) -> Tuple[int, int, List[int]]:
     """
     Read a Boolean network from a text file:
         Line 1: number of state variables
         Line 2: number of control inputs
-        Line 4: transition matrix of the network (linear representation of a logical matrix)
-        ...
+        Line 3: transition matrix of the network (linear representation of a logical matrix)
 
     :param file: a text file
     :return: (n, m, L), where
         n: number of state variables
         m: number of control inputs
-        Ls: a list of transition matrices, each for one sub-system
+        L: network transition matrix
     """
     with open(file, 'r') as f:
         n = int(f.readline().strip())
@@ -27,9 +25,10 @@ def read_network(file: str) -> Tuple[int, int, List[int]]:
         N = 2 ** n
         M = 2 ** m
         line = f.readline().strip()
-        assert line, f'{w} transition matrices must be provided!'
+        assert line, f'network transition matrix must be provided!'
         numbers = line.split()
         assert len(numbers) == M * N, f'The transition matrix must have {M * N} columns'
         L = [int(num) for num in numbers]
+        for i in L:
+            assert 1 <= i <= N, f'All integers in the network transition matrix must be in range [1, {N}]'
         return n, m, L
-
